3bea595949a3f0d2d1fae463b4ef2685b9fe3558
Sagar <Sagar@SAGAR>
2020-08-01 21:18:11 +0530

    New edmx,model added

---
+++ DemoWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DemoWebApi.Models;
+using DemoWebApi.Data;
+using System.Web.Mvc;
+
+namespace DemoWebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IHttpActionResult GetEmployee()
+        {
+            try
+            {
+                using (var obj = new myDBEntities())
+                {
+                    var results = obj.Employees.ToList();
+                    return Ok(results);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+       
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult AddEmp(EmpModel empmodel)
+        {
+            try
+            {
+                using (var obj = new myDBEntities())
+                {
+                    Employee emp = new Employee();
+                    emp.Name = empmodel.Name;
+                    emp.Email = empmodel.Email;
+                    emp.Location = empmodel.Location;
+                    emp.Designation = empmodel.Designation;
+                    obj.Employees.Add(emp);
+                    obj.SaveChanges();
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ DemoWebApi/Models/EmpModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoWebApi.Models
+{
+    public class EmpModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public string Designation { get; set; }
+    }
+}
