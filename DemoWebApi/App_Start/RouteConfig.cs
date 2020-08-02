2d35ebb001e7b17ded6a3ddd2f40d96a8b0c0a07
Sagar <Sagar@SAGAR>
2020-08-03 00:30:55 +0530

    Remove extra controller

--- DemoWebApi/App_Start/RouteConfig.cs
+++ DemoWebApi/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DemoWebApi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Employee", action = "ListEmployee", id = UrlParameter.Optional }
             );
         }
     }
--- DemoWebApi/Controllers/EmployeeController.cs
+++ DemoWebApi/Controllers/EmployeeController.cs
@@ -1,47 +1,91 @@
-﻿using System;
+﻿using DemoWebApi.Data;
+using DemoWebApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using DemoWebApi.Models;
-using DemoWebApi.Data;
-using System.Web.Mvc;
 
 namespace DemoWebApi.Controllers
 {
     public class EmployeeController : ApiController
     {
-        public IHttpActionResult GetEmployee()
-        {
-            try
-            {
-                using (var obj = new myDBEntities())
-                {
-                    var results = obj.Employees.ToList();
-                    return Ok(results);
-                }
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
-        }
-       
-        [System.Web.Http.HttpPost]
-        public IHttpActionResult AddEmp(EmpModel empmodel)
+        [HttpGet]
+        [Route("ListEmployee")]
+        public IHttpActionResult ListEmployee()
         {
             try
             {
                 using (var obj = new myDBEntities())
                 {
                     Employee emp = new Employee();
-                    emp.Name = empmodel.Name;
-                    emp.Email = empmodel.Email;
-                    emp.Location = empmodel.Location;
-                    emp.Designation = empmodel.Designation;
-                    obj.Employees.Add(emp);
+
+                    var result = obj.Employees.ToList();
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        [HttpGet]
+        [Route("GetEmployee")]
+        public IHttpActionResult GetEmployee(int? id)
+        {
+            try
+            {
+                using (var obj = new myDBEntities())
+                {
+                    List<Employee> emp = new List<Employee>();
+                    if (id != null)
+                    {
+                        emp = obj.Employees.Where(x => x.EmployeeId == id).ToList();
+                    }
+                    else
+                    {
+                        emp = obj.Employees.ToList();
+                    }
+                    return Ok(emp);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Add edit Employee
+        [HttpPost]
+        [Route("AddEditEmp")]
+        public IHttpActionResult AddEditEmp(EmpModel empmodel)
+        {
+            try
+            {
+                using (var obj = new myDBEntities())
+                {
+                    Employee emp = new Employee();
+                    emp = obj.Employees.Where(x => x.EmployeeId == empmodel.EmployeeId).FirstOrDefault();
+                    if (emp == null)
+                    {
+
+                        emp.Name = empmodel.Name;
+                        emp.Email = empmodel.Email;
+                        emp.Location = empmodel.Location;
+                        emp.Designation = empmodel.Designation;
+                        obj.Employees.Add(emp);
+                    }
+                    else
+                    {
+                        emp.EmployeeId = empmodel.EmployeeId;
+                        emp.Name = empmodel.Name;
+                        emp.Email = empmodel.Email;
+                        emp.Location = empmodel.Location;
+                        emp.Designation = empmodel.Designation;
+                    }
                     obj.SaveChanges();
                     return Ok();
                 }
@@ -50,6 +94,27 @@ namespace DemoWebApi.Controllers
             {
 
                 throw ex;
+            }
+        }
+
+        // Delete Employee
+        [HttpGet]
+        [Route("Delete")]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                using (var obj = new myDBEntities())
+                {
+                    var empDel = obj.Employees.Find(id);
+                    obj.Employees.Remove(empDel);
+                    obj.SaveChanges();
+                    return Ok();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
     }
